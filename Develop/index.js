0195280352ba920aaba8b665f7abe1acf05f46fd
Matthew Swineford <m.swineford@yahoo.com>
2022-10-18 15:11:54 -0400

    added the questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the projects name?",
+    name: "project name",
+  },
+  {
+    type: "input",
+    message: "Give the README file a discription.",
+    name: "project description",
+  },
+  {
+    type: "input",
+    message: "Provide the installation instructions.",
+    name: "installation instructions",
+  },
+  {
+    type: "input",
+    message: "Provide the usage instructions.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Provide the contribution guidelines.",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Provide the instructions for testing.",
+    name: "test",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
